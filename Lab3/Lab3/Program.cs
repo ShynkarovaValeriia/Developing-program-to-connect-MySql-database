0322bf99a3c5a84a892cf78e9be6775efc4d8336
ShynkarovaValeriia <168125704+ShynkarovaValeriia@users.noreply.github.com>
2025-04-22 11:55:37 +0300

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+                QueryEmployee(conn);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            { 
+                conn.Close();
+                conn.Dispose();
+            }
+
+            Console.Read();
+        }
+
+        private static void QueryEmployee(MySqlConnection conn)
+        {
+            string id, name, country;
+            string sql = "select Code, Name, Continent from country";
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        id = reader["Code"].ToString();
+                        name = reader["Name"].ToString();
+                        country = reader["Continent"].ToString();
+                        Console.WriteLine(new string('-', 50));
+                        Console.WriteLine("Код: " + id + " Назва: " + name + " Континент: " + country);
+                        Console.WriteLine(new string('-', 50));
+                    }
+                }
+            }
+
+        }
+    }
+}
